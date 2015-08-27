57f29380bfcde1270726494f3a9b707ba14ed02e
Simone Gonzalez <simone.b.gonzalez@gmail.com>
2015-08-27 14:36:13 -0500

    add socket.io dependencies

--- server/app.js
+++ server/app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var socketio = require('socket.io');
+
 var app = express();
 
 app.use(express.static('client'));
@@ -6,3 +8,5 @@ app.use(express.static('client'));
 app.listen(3000, function() {
   console.log('Server listening on port 3000');
 });
+
+socketio(server);
